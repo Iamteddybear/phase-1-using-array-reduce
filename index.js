fc0e1de6265071e6db15e054a74aea5be7ccdafd
ned <nedschultz@outlook.com>
2022-12-09 14:32:00 +1000

    finished assignment

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+
+let sum = 0;
+for (const batch of batteryBatches) {
+    sum = sum + batch;
+}
+
+let totalBatteries = batteryBatches.reduce(function(partialSum, currentNum) {
+    return partialSum + currentNum;
+}, 0);
